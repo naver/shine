d3f26715634638528b61530e4f2fd151a994691e
miu <mingxuanliu.miu@gmail.com>
2024-07-18 12:25:59 +0200

    fixed classifier file name issue in scripts_local/CoDet/inat and scripts_slurm/CoDet/inat

--- scripts_local/CoDet/inat/rn50/inat_codet_rn50_shine_gt.sh
+++ scripts_local/CoDet/inat/rn50/inat_codet_rn50_shine_gt.sh
@@ -14,7 +14,7 @@ python train_net_codet.py \
         DATASETS.TEST "('inat_val_l1', 'inat_val_l2', 'inat_val_l3','inat_val_l4','inat_val_l5','inat_val_l6',)" \
         MODEL.WEIGHTS ${MODEL_R50} \
         MODEL.RESET_CLS_TESTS True \
-        MODEL.TEST_CLASSIFIERS "('${METADATA_ROOT}/inat_clip_a+cname_hrchy_l1.npy', '${METADATA_ROOT}/inat_clip_a+cname_hrchy_l2.npy', '${METADATA_ROOT}/inat_clip_a+cname_hrchy_l3.npy', '${METADATA_ROOT}/inat_clip_a+cname_hrchy_l4.npy', '${METADATA_ROOT}/inat_clip_a+cname_hrchy_l5.npy', '${METADATA_ROOT}/inat_clip_a+cname_hrchy_l6.npy', )" \
+        MODEL.TEST_CLASSIFIERS "('${METADATA_ROOT}/inat_clip_hrchy_l1.npy', '${METADATA_ROOT}/inat_clip_hrchy_l2.npy', '${METADATA_ROOT}/inat_clip_hrchy_l3.npy', '${METADATA_ROOT}/inat_clip_hrchy_l4.npy', '${METADATA_ROOT}/inat_clip_hrchy_l5.npy', '${METADATA_ROOT}/inat_clip_hrchy_l6.npy', )" \
         MODEL.TEST_NUM_CLASSES "(5, 18, 61, 184, 317, 500,)" \
         MODEL.MASK_ON False
 
